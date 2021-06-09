d572bb972968d5ec1874aa875f16721a55ae8558
Tatiroa994 <tatico994@gmail.com>
2021-06-09 18:02:32 -0500

    firebase 2

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { RoleBtnComponent } from './components/role-btn/role-btn.component';
+import { ButtomComponent } from './components/buttom/buttom.component';
 
 @NgModule({
-  declarations: [HeaderComponent, RoleBtnComponent],
+  declarations: [HeaderComponent, RoleBtnComponent, ButtomComponent],
   imports: [CommonModule],
-  exports: [HeaderComponent, RoleBtnComponent],
+  exports: [HeaderComponent, RoleBtnComponent, ButtomComponent],
+  
 })
 export class SharedModule {}
